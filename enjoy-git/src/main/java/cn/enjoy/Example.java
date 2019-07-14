e54353b3627e2a996f0d164110fcdb7616152b3f
xiaotangtang0318 <zhangwei_625@163.com>
2019-07-14 16:08:05 +0800

    newgit

---
+++ enjoy-git/src/main/java/cn/enjoy/Example.java
@@ -0,0 +1,16 @@
+package cn.enjoy;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello my first git!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
