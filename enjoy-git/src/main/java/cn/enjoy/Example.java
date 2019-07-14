9e25bca14bed6379c90c84252590990894e9c6c6
xiaotangtang0318 <zhangwei_625@163.com>
2019-07-14 16:29:23 +0800

    king

--- enjoy-git/src/main/java/cn/enjoy/Example.java
+++ enjoy-git/src/main/java/cn/enjoy/Example.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 public class Example {
     @RequestMapping("/")
     String home() {
-        return "Hello my first git!";
+        return "Hello my first git! -king";
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
